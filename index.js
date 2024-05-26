10a87d1d4586a4b8e4b1a00a99f86520c184315b
V1kyCh <126974500+V1kyCh@users.noreply.github.com>
2024-05-26 12:35:36 +0300

    done

---
+++ index.js
@@ -0,0 +1,17 @@
+const testA = 5 + 5 + "5";
+console.log(typeof testA);
+
+const email = "celnakv@gmail.com";
+console.log(email.includes("@"));
+console.log(email.length);
+
+const my = "My";
+const forename = "name";
+const is = "is";
+const fullName = my + " " + forename + " " + is;
+const victor = "Victor";
+console.log(fullName + " " + victor);
+
+const userName = "Вікторія";
+const payment = "348грн";
+alert("Дякуємо, " + userName + "! До сплати " + payment);
